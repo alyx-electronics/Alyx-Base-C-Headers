958b02890f862d3ad8f6d86abdac07ef774e0f0e
Alex <130048160+bauer-alex-174@users.noreply.github.com>
2024-03-22 11:16:32 +0100

    Create stdlib.h
    Signed-off-by: Alex <130048160+bauer-alex-174@users.noreply.github.com>

---
+++ src/alyx_stdlib/include/alyx/stdlib.h
@@ -0,0 +1,15 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+void alyx_init_serial() {
+    stdio_init_all();
+}
+
+unsigned char* alyx_scanf() {
+    unsigned char buffer[85];
+    unsigned char u , *p ;
+    for (p=buffer, u = getchar() ; u != EOF && u != '\r' && p - buffer <80 ; u = getchar()) putchar(*p++=u);
+    *p = 0 ;
+    printf("\n");
+    return buffer;
+}
