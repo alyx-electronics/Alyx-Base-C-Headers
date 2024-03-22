e0449f0cf44359f5ae941488de894b71f62c5c47
Alex <130048160+bauer-alex-174@users.noreply.github.com>
2024-03-22 12:12:53 +0100

    Update stdlib.h
    Signed-off-by: Alex <130048160+bauer-alex-174@users.noreply.github.com>

--- src/alyx_stdlib/include/alyx/stdlib.h
+++ src/alyx_stdlib/include/alyx/stdlib.h
@@ -5,11 +5,9 @@ void alyx_init_serial() {
     stdio_init_all();
 }
 
-unsigned char* alyx_scanf() {
-    unsigned char buffer[85];
+void alyx_scanf(unsigned char buffer[85]) {
     unsigned char u , *p ;
     for (p=buffer, u = getchar() ; u != EOF && u != '\r' && p - buffer <80 ; u = getchar()) putchar(*p++=u);
     *p = 0 ;
     printf("\n");
-    return buffer;
 }
